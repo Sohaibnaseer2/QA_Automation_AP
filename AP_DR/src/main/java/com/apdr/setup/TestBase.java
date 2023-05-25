be996fec9fe5bdcbb4462c21725656c7900325b4
Sohaib Naseer <sohaibnaseer2@gmail.com>
2023-05-25 16:08:12 +0500

    Change path of Chrome Signed-off-by: Sohaib Naseer <sohaibnaseer2@gmail.com>

--- AP_DR/src/main/java/com/apdr/setup/TestBase.java
+++ AP_DR/src/main/java/com/apdr/setup/TestBase.java
@@ -43,7 +43,7 @@ public class TestBase {
     public void initialization() {
         String browserName = prop.getProperty("browser");
         if (browserName.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "chrome/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "C:/Users/sohaib.naseer/git/repository/AP_DR/chrome/chromedriver.exe");
 //            ChromeOptions options = new ChromeOptions();
 //            options.addArguments("--lang= locale-of-choice");
             driver = new ChromeDriver();
