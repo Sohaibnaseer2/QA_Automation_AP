65d75937273792c551c264aa778c05c44e160a22
Sohaib Naseer <sohaibnaseer2@gmail.com>
2023-05-30 20:29:17 +0500

    Change file path Signed-off-by: Sohaib Naseer <sohaibnaseer2@gmail.com>

--- AP_DR/src/main/java/com/apdr/setup/TestBase.java
+++ AP_DR/src/main/java/com/apdr/setup/TestBase.java
@@ -51,11 +51,10 @@ public class TestBase {
         if (browserName.equals("chrome")) {
         	//WebDriverManager.chromedriver().setup();
 			
-			//ChromeOptions options = new ChromeOptions(); 
-			 //options.setHeadless(true);
-			
-		
-			//options.addArguments("--Headless=new");
+			ChromeOptions options = new ChromeOptions(); 
+			options.addArguments("--Headless=new");
+			driver = new ChromeDriver(options);
+			//options.setHeadless(true);
 			/*
 			 * options.addArguments("--disable-gpu");
 			 * options.addArguments("--window-size=800,600");
@@ -63,7 +62,7 @@ public class TestBase {
 			 */
 			 
        
-            driver = new ChromeDriver();
+           
 
         }
 
